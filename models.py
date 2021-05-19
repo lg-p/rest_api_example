b02df745dd6a4062fd3e3a3eb6949f1fafc1c0d7
alena <alena@lagoshny.ru>
2021-05-19 10:18:54 +0300

    user authentication

--- models.py
+++ models.py
@@ -1,4 +1,8 @@
+from datetime import timedelta
+
 from sqlalchemy import Column, Integer, String
+from flask_jwt_extended import create_access_token
+from passlib.hash import bcrypt
 
 from app import Base
 
@@ -8,3 +12,19 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     login = Column(String(250), nullable=False, unique=True)
     password = Column(String(120), nullable=False)
+
+    def __init__(self, **kwargs):
+        self.login = kwargs.get('login')
+        self.password = bcrypt(kwargs.get('password'))
+
+    def get_token(self, expire_time=24):
+        expire_delta = timedelta(expire_time)
+        token = create_access_token(identity=self.id, expires_delta=expire_delta)
+        return token
+
+    @classmethod
+    def authenticate(cls, login, password):
+        user = cls.query.filter(cls.login == login)
+        if not bcrypt.verify(password, user.password):
+            raise Exception("No user with this password")
+        return user
