2e6bc1902e05c7768a08f28279804d3efd83f85a
alena <alena@lagoshny.ru>
2021-05-24 16:15:44 +0300

    added validation for users registration

--- app/registration/view.py
+++ app/registration/view.py
@@ -1,19 +1,22 @@
-from flask import request, jsonify
+from flask import jsonify
+from flask_apispec import use_kwargs, marshal_with
 
 from app.registration import bp_reg
 from models import User
 from app import session
+from schemes import UserSchema
 
 
 @bp_reg.route('/registration', methods=['POST'])
-def registration():
-    param = request.json
-    user_login = param.get('login')
+@use_kwargs(UserSchema)
+@marshal_with(UserSchema)
+def registration(**kwargs):
+    user_login = kwargs.get('login')
 
     if User.user_exists(user_login):
         raise Exception("User with this login already exists")
 
-    user = User(**param)
+    user = User(**kwargs)
     try:
         session.add(user)
         session.commit()
